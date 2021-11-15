775c136645c62dac0c3a28d2285f22658a22c836
IvanEarth <gercevi@mail.ru>
2021-11-15 20:02:42 +0200

    Add print solver method

--- libs/SolverTable.cpp
+++ libs/SolverTable.cpp
@@ -4,6 +4,44 @@
 
 #include "SolverTable.h"
 #include "vector"
+#include <iostream>
 
-SolverTable::SolverTable()= default;
+SolverTable::SolverTable()
+{
+    this->table = new std::vector<std::vector<int>>();
+    this->table->resize(0);
+}
 
+PrintSolverTable::PrintSolverTable(SolverTable *table)
+{
+    SolverTable *table_ = table;
+}
+
+
+std::vector<std::vector<int>> SolverTable::getTable() const
+{
+    return *this->table;
+}
+
+SolverTable::~SolverTable()
+{
+    delete this->table;
+}
+
+void PrintSolverTable::printTable()
+{
+    std::cout << "Table:" << std::endl;
+    for (int i = 0; i < solverTable->getTable().size(); i++)
+    {
+        for (int j = 0; j < solverTable->getTable()[i].size(); j++)
+        {
+            std::cout << solverTable->getTable()[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+PrintSolverTable::~PrintSolverTable()
+{
+    delete solverTable;
+}
--- libs/SolverTable.h
+++ libs/SolverTable.h
@@ -5,13 +5,31 @@
 #ifndef SIMPLEXSOLVER_SOLVERTABLE_H
 #define SIMPLEXSOLVER_SOLVERTABLE_H
 
-class SolverTable {
+#include <vector>
+
+class SolverTable
+{
 private:
+    std::vector<std::vector<int>> *table;
 public:
     SolverTable();
+
     ~SolverTable();
 
+    std::vector<std::vector<int>> getTable() const;
 
+};
+
+class PrintSolverTable
+{
+private:
+    SolverTable *solverTable{};
+public:
+    explicit PrintSolverTable(SolverTable *table);
+
+    ~PrintSolverTable();
+
+    void printTable();
 };
 
 #endif //SIMPLEXSOLVER_SOLVERTABLE_H
